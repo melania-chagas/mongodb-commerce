c25b443135f19a12b62f0882257618a4246f1ba0
Melania P Chagas <melaniapchagas@gmail.com>
2022-12-15 20:43:49 -0300

    Refatorando query anterior

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,4 +1,6 @@
+// O operador $elemMatch seleciona os documentos que contêm um campo do tipo array com pelo menos um elemento que satisfaça todos os critérios de seleção especificados.
+
 db.produtos.find({
-  "valoresNutricionais.0.quantidade": { $lt: 500 },
-},
-{ nome: true, _id: false });
+  valoresNutricionais:
+  { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } },
+}, { nome: 1, _id: 0 });
