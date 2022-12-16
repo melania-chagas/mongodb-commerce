c806f93e0cd5cc15ff6ecf60371a397d46008111
Melania P Chagas <melaniapchagas@gmail.com>
2022-12-16 11:48:19 -0300

    Query que retorna todos os lanches que possuem 'picles' nos ingredientes e mostra os 3 primeiros itens do array 'valoresNutricionais'

---
+++ challenges/desafio14.js
@@ -0,0 +1,8 @@
+// $slice para retornar, neste caso, os 3 primeiros elementos ordenados no array 'valoresNutricionais'.
+
+// O operador $elemMatch seleciona os documentos que contêm um campo do tipo array com pelo menos um elemento que satisfaça todos os critérios de seleção especificados.
+
+db.produtos.find(
+  { ingredientes: { $elemMatch: { $eq: "picles" } } },
+  { nome: true, ingredientes: true, valoresNutricionais: { $slice: 3 }, _id: false },
+  );
