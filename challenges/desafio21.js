b4307ce52934238eba8b94284387c5eb2a8793ed
Melania P Chagas <melaniapchagas@gmail.com>
2022-12-19 18:10:11 -0300

    Query que remove o último ingrediente do sanduíche 'Cheddar McMelt'

---
+++ challenges/desafio21.js
@@ -0,0 +1,12 @@
+// Uma maneira simples de remover o primeiro ou o último elemento de um array é utilizar o operador $pop.
+// Passando o valor -1 ao operador $pop você removerá o primeiro elemento. Já ao passar o valor 1, você removerá o último elemento do array. 
+
+db.produtos.updateMany(
+  { nome: { $eq: "Cheddar McMelt" } },
+  { $pop: { ingredientes: 1 } },
+); 
+
+db.produtos.find(
+  {},
+  { _id: false, nome: true, ingredientes: true },
+);
